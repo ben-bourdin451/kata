9aab36cf1139674652068dab60661e3d6b98523c
Ben Bourdin <ben@grabyo.com>
2019-12-08 10:38:08 +0000

    2019 day 5

---
+++ adventofcode/2019/day5.go
@@ -0,0 +1,15 @@
+package adventofcode
+
+func day5(mem string, input int) int {
+	memory := initCodes(mem)
+
+	_, output := intcode(memory, input)
+
+	for _, out := range output {
+		if out != 0 {
+			return out
+		}
+	}
+
+	return -1
+}
---
+++ adventofcode/2019/day5_test.go
@@ -0,0 +1,34 @@
+package adventofcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDay5Part1Final(t *testing.T) {
+	in, err := readStrings("./day5_input.txt")
+	if err != nil {
+		t.Error("Error while reading input", err)
+	}
+
+	got := day5(in[0], 1)
+	want := 15508323
+	fmt.Printf("Day 5, part 1 answer: %v\n", got)
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestDay5Part2Final(t *testing.T) {
+	in, err := readStrings("./day5_input.txt")
+	if err != nil {
+		t.Error("Error while reading input", err)
+	}
+
+	got := day5(in[0], 5)
+	want := 9006327
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+	fmt.Printf("Day 5, part 2 answer: %v\n", got)
+}
