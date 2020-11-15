3c5478a2f5f1eb0f8dd15745c08fa72d7a0362df
Ben Bourdin <ben.bourdin451@gmail.com>
2020-11-15 16:04:31 +0000

    add graph cci questions

---
+++ cci/graphs_test.go
@@ -0,0 +1,19 @@
+package cci
+
+import "testing"
+
+func TestGraphMinimalTree(t *testing.T) {
+	cases := []struct {
+		name string
+	}{
+		{
+			"given ",
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+
+		})
+	}
+}
